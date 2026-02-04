87eb98df6e6ce9c0452e2e36a3d077915619dea5
anil <anilkumarguntapelli@gmail.com>
2026-02-04 16:18:04 +0530

    terraform project

---
+++ day-2/terraform.tfvars
@@ -0,0 +1,4 @@
+region = "us-east-1"
+ami = "ami-0532be01f26a3de55"
+instance_type = "t3.micro"
+key_name = "hydro"
---
+++ day-2/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    default = ""
+}
+
+variable "instance_type" {
+    default = ""
+}
+variable "ami" {
+  default = ""
+  
+}
+variable "key_name" {
+    default = ""
+}
