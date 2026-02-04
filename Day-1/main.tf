2a20aa2e0ab13c3305b609ffa7c3f7c1c44600ff
anil <anilkumarguntapelli@gmail.com>
2026-02-04 15:06:57 +0530

    Initial Terraform project (clean)

---
+++ Day-1/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-0532be01f26a3de55"
+    instance_type = "t3.micro"
+    key_name = "hydro"
+    
+}
